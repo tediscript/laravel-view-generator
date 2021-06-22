7093381de1cc91ee6d4ca3afcb49fa6a487d3de5
Alfredo Pareto <tediscript@gmail.com>
2021-06-22 17:20:32 +0700

    Create ViewGeneratorServiceProvider.php

---
+++ ViewGeneratorServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tediscript\ViewGenerator;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class ViewGeneratorServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            Console\MakeViewCommand::class,
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Console\MakeViewCommand::class];
+    }
+}
